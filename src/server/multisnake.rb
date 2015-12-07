2f645d06885aa3abd3ea108cd71d6f5acaac64b9
jvalen <javiervalenciaromero@gmail.com>
2015-12-07 16:41:35 -0500

    Board boundaries bug fix

--- src/server/multisnake.rb
+++ src/server/multisnake.rb
@@ -1,66 +1,68 @@
 class MultisnakeGame
-	attr_accessor :center, :size, :key_code, :BLOCK_SIZE
+  attr_accessor :center, :size, :key_code, :BLOCK_SIZE
 
-	def initialize()
-		@SCREEN_HEIGHT = 500
-		@SCREEN_WIDTH = 500
-		@BLOCK_SIZE = 10
-		@size = {:x => @SCREEN_WIDTH, :y => @SCREEN_HEIGHT}
-		@center = {:x => @size[:x] / 2, :y => @size[:y] / 2}
+  def initialize()
+    @SCREEN_HEIGHT = 310
+    @SCREEN_WIDTH = 310
+    @BLOCK_SIZE = 10
+    @size = {:x => @SCREEN_WIDTH, :y => @SCREEN_HEIGHT}
+    @center = {:x => @size[:x] / 2, :y => @size[:y] / 2}
 
-		@bodies = []
-		@bodies << HeadBlock.new(self)
-		add_food
+    @bodies = []
+    @bodies << HeadBlock.new(self)
+    add_food
 
-	end
+  end
 
-	def add_food
+  def add_food
     add_body(FoodBlock.new(self))
-	end
+  end
 
   def add_body(body)
     @bodies << body
   end
 
-	# In the JavaScript version, we ultimately got keyboard state
-	# from the Keyboarder object that was created on the headblock.
-	# For now, we need to get the updates from the client, and we
-	# get information from the client via the websocket connection
-	# which speaks to the server. Passing it through tick. Not sure
-	# if this is an unwise design choice.
-	def tick(key_code)
-		@key_code = key_code
+  # In the JavaScript version, we ultimately got keyboard state
+  # from the Keyboarder object that was created on the headblock.
+  # For now, we need to get the updates from the client, and we
+  # get information from the client via the websocket connection
+  # which speaks to the server. Passing it through tick. Not sure
+  # if this is an unwise design choice.
+  def tick(key_code)
+    @key_code = key_code
 
-		update
-		get_state
-	end
+    update
+    get_state
+  end
 
-	def update
-		@bodies.each do |body|
-			body.update if body.class.method_defined?(:update)
-		end
+  def update
+    @bodies.each do |body|
+      body.update if body.class.method_defined?(:update)
+    end
 
-		# Report collisions
-	end
+    # Report collisions
+  end
 
-	def get_state
-		mockup_data = {}
-		mockup_data[:board] = {:size => {:width => 310, :height => 310}}
+  def get_state
+    mockup_data = {}
+    mockup_data[:board] = {:size => {:width => 310, :height => 310}}
 
-		snakes = []
-		@bodies.each do |body|
-			snake_object = {}
-			snake_object[:headblock] = body.get_object
-			snake_object[:bodyblocks] = []
-			snakes << snake_object
-		end
+    snakes = []
+    @bodies.each do |body|
+      snake_object = {}
+      snake_object[:headblock] = body.get_object
+      snake_object[:bodyblocks] = []
+      snakes << snake_object
+    end
 
-		mockup_data[:snakes] = snakes
-		mockup_data
-	end
+    mockup_data[:snakes] = snakes
+    mockup_data
+  end
 
   def random_square
     rand_generator = Random.new()
+
+
 
     {:x => (@size[:x] / @BLOCK_SIZE * rand_generator.rand).floor * @BLOCK_SIZE + @BLOCK_SIZE / 2,
      :y => (@size[:y] / @BLOCK_SIZE * rand_generator.rand).floor * @BLOCK_SIZE + @BLOCK_SIZE / 2}
@@ -113,55 +115,55 @@ class FoodBlock
 end
 
 class HeadBlock
-	attr_accessor :center, :size, :color
+  attr_accessor :center, :size, :color
 
-	def initialize(game)
-		@game = game
-		@BLOCK_SIZE = @game.BLOCK_SIZE
-		@center = {:x => @game.center[:x], :y => @game.center[:y]}
-		@direction = {:x => 1, :y => 0}
-		@size = {:x => @BLOCK_SIZE, :y => @BLOCK_SIZE}
-		@blocks = []
+  def initialize(game)
+    @game = game
+    @BLOCK_SIZE = @game.BLOCK_SIZE
+    @center = {:x => @game.center[:x], :y => @game.center[:y]}
+    @direction = {:x => 1, :y => 0}
+    @size = {:x => @BLOCK_SIZE, :y => @BLOCK_SIZE}
+    @blocks = []
 
-		@last_move = Time.now
-		@add_block = false
-	end
+    @last_move = Time.now
+    @add_block = false
+  end
 
-	def update
-		handle_keyboard(@game.key_code)
+  def update
+    handle_keyboard(@game.key_code)
 
-		now = Time.now
-		if (now > @last_move)
-			move
-			@last_move = now
-		end
-	end
+    now = Time.now
+    if (now > @last_move)
+      move
+      @last_move = now
+    end
+  end
 
-	def handle_keyboard(key_code)
-		@KEYS = {:LEFT => 37, :RIGHT => 39, :UP => 38, :DOWN => 40}
+  def handle_keyboard(key_code)
+    @KEYS = {:LEFT => 37, :RIGHT => 39, :UP => 38, :DOWN => 40}
 
-		if key_code == @KEYS[:LEFT]
-			@direction[:x] = -1
-			@direction[:y] = 0			
-		elsif key_code == @KEYS[:RIGHT]
-			@direction[:x] = 1
-			@direction[:y] = 0
-		elsif key_code == @KEYS[:UP]
-			@direction[:y] = -1
-			@direction[:x] = 0
-		elsif key_code == @KEYS[:DOWN]
-			@direction[:y] = 1
-			@direction[:x] = 0
-		end
-	end
+    if key_code == @KEYS[:LEFT]
+      @direction[:x] = -1
+      @direction[:y] = 0
+    elsif key_code == @KEYS[:RIGHT]
+      @direction[:x] = 1
+      @direction[:y] = 0
+    elsif key_code == @KEYS[:UP]
+      @direction[:y] = -1
+      @direction[:x] = 0
+    elsif key_code == @KEYS[:DOWN]
+      @direction[:y] = 1
+      @direction[:x] = 0
+    end
+  end
 
-	def move
-		prev_block_center = {:x => @center[:x], :y => @center[:y]}
-		@center[:x] += @direction[:x] * @BLOCK_SIZE
-		@center[:y] += @direction[:y] * @BLOCK_SIZE
-	end
+  def move
+    prev_block_center = {:x => @center[:x], :y => @center[:y]}
+    @center[:x] += @direction[:x] * @BLOCK_SIZE
+    @center[:y] += @direction[:y] * @BLOCK_SIZE
+  end
 
-	def get_object
-		{:center => @center, :color => "red", :size => {:x => 10, :y => 10}}
-	end
+  def get_object
+    {:center => @center, :color => "red", :size => {:x => 10, :y => 10}}
+  end
 end
