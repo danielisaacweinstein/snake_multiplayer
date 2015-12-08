1beb3cf725095633090d621dab06eb68b37a3e43
Daniel Isaac Weinstein <danielisaacweinstein@gmail.com>
2015-12-08 16:22:43 -0500

    Backing up files

--- src/server/snake_server.rb
+++ src/server/snake_server.rb
@@ -4,32 +4,50 @@ require 'json'
 require 'bundler'
 Bundler.require
 
-Faye::WebSocket.load_adapter('thin')
-
-get '/' do
-  if Faye::WebSocket.websocket?(request.env)
-    ws = Faye::WebSocket.new(request.env)
-
-    ws.on(:open) do |event|
-      # check what's already running
-      @game = MultisnakeGame.new()
-    end
-
-    ws.on(:message) do |msg|
-      key_code = msg.data.to_i
-
-      state = @game.tick(key_code)
-      json_game_state = JSON.generate(state)
-      # puts json_game_state
-      ws.send(json_game_state)
-    end
-
-    ws.on(:close) do |event|
-      puts 'On Close'
-    end
-
-    ws.rack_response
-  else
-    # erb :index
+class ServerClass < Sinatra::Base
+  def initialize
+    Faye::WebSocket.load_adapter('thin')
+    # @game
+    puts "LINE 11"
   end
+
+  puts "LINE 14"
+  
+  get '/' do
+    if Faye::WebSocket.websocket?(request.env)
+      ws = Faye::WebSocket.new(request.env)
+
+      ws.on(:open) do |event|
+        # puts @game
+        # if !defined?(@game)
+          @game = MultisnakeGame.new()
+        # else
+        #   puts "ALREADY GAME"
+        # end
+      end
+
+      ws.on(:message) do |msg|
+        key_code = msg.data.to_i
+
+        state = @game.tick(key_code)
+        json_game_state = JSON.generate(state)
+        # puts json_game_state
+        ws.send(json_game_state)
+      end
+
+      ws.on(:close) do |event|
+        puts 'On Close'
+      end
+
+      ws.rack_response
+    else
+      # erb :index
+    end
+  end
+
+  puts "LINE 46"
+  run! if app_file == $0
+  puts "LINE 47"
 end
+
+ServerClass.new
